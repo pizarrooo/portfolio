9229c76c906f9b7ddc571782ba0a6761da15715f
Andrea Nicole Pizarro <nicolepizarro923.gmail.com>
2026-04-13 23:07:54 +0800

    Update about me page

--- script.js
+++
@@ -1,6 +0,0 @@
-fetch('about-me-text.json')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById("about-me-text");
-    container.textContent = data.about;
-  });
